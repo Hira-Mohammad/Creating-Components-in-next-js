d997c1e71e1386bb0f7d4422fa522bc5bf4b5ae8
Rizwana Shireen <161503313+Hira-Mohammad@users.noreply.github.com>
2024-10-20 21:46:20 +0500

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,12 +1,18 @@
+
+// Create 2 components  which name  is header.tsx and about.tsx and import it on homepage.
 import React from "react"
 import Link from "next/link"
-export default function Header(){
-    return(
-      <div>
-        <h1 className='bg-blue-400 text-black p-4pt '>This is my Header</h1>
-        <Link href={"/home"}>Home</Link>
+import Header from "./header/page"
+import About from"./about/page"
+export default function Home(){
+  return(
+    <div>
+      
+      <h1 className='bg-blue-400 text-black p-4pt '>Home</h1>
+      <Link href={"/header"}>Header</Link>
       <br></br>
       <Link href={"/about"}>About</Link>
-      </div>
-    )
-  };
+      
+    </div>
+  )
+};
